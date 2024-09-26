3cc8f2e2ac0f40e034f862679f2b3ea72661595f
+ <+>
2024-09-26 21:04:50 +0300

    Arrow functions lab

---
+++ index.js
@@ -0,0 +1,10 @@
+const divide = function(x, y){
+    return x / y
+}
+console.log(divide(2000, 100))
+
+const square = num => num * num
+console.log(square)
+
+const add = (num1, num2) => num1 + num2
+console.log(add)
